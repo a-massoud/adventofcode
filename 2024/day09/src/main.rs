d0df25578b28deb96a57ac86a2acd512acfb735e
a-massoud <purple.ultraviolet616@gmail.com>
2024-12-15 14:11:52 -0700

    Optimize 2024 day 9

--- 2024/day09/src/main.rs
+++ 2024/day09/src/main.rs
@@ -1,6 +1,7 @@
-// Ok, this is the first one this year that is still slow in release mode. Not too proud of this.
+// Probably still far from optimal, but I like it
 
 use anyhow::{anyhow, bail};
+use std::collections::BTreeMap;
 use std::{env, fs, iter};
 
 fn main() -> anyhow::Result<()> {
@@ -18,7 +19,7 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn parse_input(input: &str) -> anyhow::Result<Vec<Option<usize>>> {
+fn parse_input(input: &str) -> anyhow::Result<BTreeMap<usize, (usize, Option<u32>)>> {
     let tmp = input
         .trim()
         .chars()
@@ -31,76 +32,101 @@ fn parse_input(input: &str) -> anyhow::Result<Vec<Option<usize>>> {
     Ok(tmp
         .into_iter()
         .enumerate()
-        .flat_map(|(i, n)| {
-            if i % 2 == 0 {
-                iter::repeat_n(Some(i / 2), n as usize)
+        .filter_map(|(i, n)| {
+            if n == 0 {
+                None
+            } else if i % 2 == 0 {
+                Some((n as usize, Some((i / 2) as u32)))
             } else {
-                iter::repeat_n(None, n as usize)
+                Some((n as usize, None))
             }
         })
-        .collect())
+        .fold((0usize, BTreeMap::new()), |(nexti, mut map), i| {
+            map.insert(nexti, i);
+            (nexti + i.0, map)
+        })
+        .1)
 }
 
-fn part1(disk: &Vec<Option<usize>>) -> usize {
-    let mut disk = disk.clone();
+fn part1(disk: &BTreeMap<usize, (usize, Option<u32>)>) -> usize {
+    let mut disk: Vec<Option<u32>> = disk
+        .iter()
+        .flat_map(|(_, (n, v))| iter::repeat_n(*v, *n))
+        .collect();
     let mut idx = 0;
     let mut edx = disk.len() - 1;
 
-    while idx <= edx && !disk[idx].is_none() {
+    while idx < edx && !disk[idx].is_none() {
         idx += 1;
     }
-    while idx <= edx {
-        while disk[edx].is_none() && idx <= edx {
+    while idx < edx {
+        while disk[edx].is_none() && idx < edx {
             edx -= 1;
         }
 
         disk.swap(idx, edx);
 
-        while idx <= edx && !disk[idx].is_none() {
+        while idx < edx && !disk[idx].is_none() {
             idx += 1;
         }
     }
 
     disk.iter()
         .enumerate()
-        .fold(0usize, |acc, (i, x)| acc + i * x.unwrap_or(0))
+        .fold(0usize, |acc, (i, x)| acc + i * (x.unwrap_or(0) as usize))
 }
 
-fn part2(disk: &Vec<Option<usize>>) -> usize {
+fn part2(disk: &BTreeMap<usize, (usize, Option<u32>)>) -> usize {
     let mut disk = disk.clone();
-    let mut edx = disk.len() - 1;
 
-    while 0 < edx {
-        while disk[edx].is_none() && 0 < edx {
-            edx -= 1;
-        }
+    let mut edx = disk.iter().rev().find(|(_, (_, v))| v.is_some());
+    while edx.is_some() && edx.unwrap().0 > &0 {
+        let block = (*edx.unwrap().0, *edx.unwrap().1);
 
-        let fend = edx;
-        let mut fstart = fend;
-        while disk[fstart] == disk[fend] && fstart > 0 {
-            fstart -= 1;
-        }
-        let fstart = fstart + 1;
-        let flen = fend - fstart + 1;
-
-        let mut ostart = 0;
-        while !disk.iter().skip(ostart).take(flen).all(|x| x.is_none()) && ostart < fstart {
-            ostart += 1;
-        }
-
-        if ostart < fstart {
-            for i in 0..flen {
-                disk.swap(ostart + i, fstart + i);
+        let idx = disk
+            .range(..block.0)
+            .find(|(_, (n, v))| v.is_none() && n >= &block.1 .0);
+        if let Some(idx) = idx {
+            let (idx, nblock) = (*idx.0, *idx.1);
+            disk.remove(&idx);
+            if nblock.0 == block.1 .0 {
+                disk.insert(idx, block.1);
+            } else {
+                disk.insert(idx, block.1);
+                disk.insert(idx + block.1 .0, (nblock.0 - block.1 .0, None));
             }
+
+            disk.remove(&block.0);
+            let mut new_null_start = block.0;
+            let mut new_null_len = block.1 .0;
+            let before = disk.range(..block.0).rev().next();
+            if let Some(before) = before {
+                let before = (*before.0, *before.1);
+
+                if before.1 .1.is_none() {
+                    disk.remove(&before.0);
+                    new_null_start = before.0;
+                    new_null_len += before.1 .0;
+                }
+            }
+            let after = disk.range(block.0..).next();
+            if let Some(after) = after {
+                let after = (*after.0, *after.1);
+
+                if after.1 .1.is_none() {
+                    disk.remove(&after.0);
+                    new_null_len += after.1 .0;
+                }
+            }
+
+            disk.insert(new_null_start, (new_null_len, None));
         }
-        if 0 < fstart {
-            edx = fstart - 1;
-        } else {
-            break;
-        }
+
+        edx = disk.range(..block.0).rev().find(|(_, (_, v))| v.is_some());
     }
 
     disk.iter()
+        .flat_map(|(_, (n, v))| iter::repeat_n(*v, *n))
         .enumerate()
-        .fold(0usize, |acc, (i, x)| acc + i * x.unwrap_or(0))
+        .fold(0usize, |acc, (i, x)| acc + i * (x.unwrap_or(0) as usize))
 }
